845a27849da8a9bde3231a92671596776368c2a8
Ranu Chandan <ranumandal01331@gmail.com>
2026-04-15 13:12:44 +0530

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,10 +1,28 @@
+class Vehicle:
+    def move(self):
+        pass
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving on the road")
+
+class Bicycle(Vehicle):
+    def move(self):
+        print("Pedaling on the road")
+
+# Example
+v1 = Car()
+v2 = Bicycle()
+
+v1.move()
+v2.move()
 import math
 
-def circle_area(radius):
+def circle(radius):
     return math.pi * radius * radius
 
-def rectangle_area(length, width):
-    return length * width
+def rectangle(l, w):
+    return l * w
 
-def triangle_area(base, height):
-    return 0.5 * base * height
+def triangle(b, h):
+    return 0.5 * b * h
